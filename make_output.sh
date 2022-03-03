516d25900e0438c1ed980c5b1b1f9063c886786a
Rusiek <szymon.rusiecki@protonmail.com>
2022-03-03 15:26:28 +0100

    Python3 can be a generator file

--- make_output.sh
+++ make_output.sh
@@ -1,4 +1,12 @@
+#!/bin/sh
+
 for file in $2/*.in; do
     output=${file%.in}.out
-    cat <(./$1 < $file) > $output
+
+    if [[ $1 == *.exe ]]; then
+        cat <(./$1 < $file) > $output
+    elif [[ $1 == *.py ]]; then
+        cat <(python3 $1 < $file) > $output
+    fi
+    
 done
