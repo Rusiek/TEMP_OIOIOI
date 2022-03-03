28cb097f66cbe72b58f58884c3a96d00baa4e0a5
Rusiek <szymon.rusiecki@protonmail.com>
2022-03-03 15:30:50 +0100

    Python3 file can be tested

--- test_program.sh
+++ test_program.sh
@@ -4,10 +4,14 @@ for file in $2/*.in; do
     start=`date +%s%N`
     test=${file%.in}.out
 
-    if [[ $1 == *.exe ]]; then
+    if      [[ $1 == *.exe ]];  then
         diff -bsq $test <(./$1 < $file) || break
-    elif [[ $1 == *.py ]]; then
+    elif    [[ $1 == *.py ]];   then
         diff -bsq $test <(python3 $1 < $file) || break
+    elif    [[ $1 == *.c ]];    then
+        printf "Use .exe file instead of .c file!\n"
+    elif    [[ $1 == *.cpp ]];  then
+        printf "Use .exe file instead of .cpp file!\n"
     fi
 
     end=`date +%s%N`
