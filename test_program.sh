5974d42c24dfc30a4d5944ab0c60d1e39583613d
Rusiek <szymon.rusiecki@protonmail.com>
2022-03-03 12:01:10 +0100

    Added running Python3 files

--- test_program.sh
+++ test_program.sh
@@ -1,7 +1,15 @@
+#!/bin/sh
+
 for file in $2/*.in; do
     start=`date +%s%N`
     test=${file%.in}.out
-    diff -bsq $test <(./$1 < $file) || break
+
+    if [[ $1 == *.exe ]]; then
+        diff -bsq $test <(./$1 < $file) || break
+    elif [[ $1 == *.py ]]; then
+        diff -bsq $test <(python3 $1 < $file) || break
+    fi
+
     end=`date +%s%N`
     printf "\n%.3f%s\n" $(((end-start)/(10**6)))e-3 s
 done
