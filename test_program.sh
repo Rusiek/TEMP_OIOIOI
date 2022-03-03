d9a668b3eef37553b3813666736bfc22375655bc
Rusiek <szymon.rusiecki@protonmail.com>
2022-03-03 19:04:37 +0100

    Added a simple raport at the end

--- test_program.sh
+++ test_program.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+TIME_MAX=0
+MEMORY_MAX=0
+
 for file in $2/*.in; do
     start=`date +%s%N`
     test=${file%.in}.out
@@ -15,5 +18,15 @@ for file in $2/*.in; do
     fi
 
     end=`date +%s%N`
-    printf "%.3f%s\n\n" $(((end-start)/(10**6)))e-3 s
+    time_s=$(( (end-start)/(10**6) ))
+    printf "%.3fs\n\n" $(( $time_s ))e-3
+
+    if (( "$time_s" > "$TIME_MAX" )); then
+        TIME_MAX=$(( $time_s ))
+        TIME_MAX_NAME=$( basename $test )
+    fi
 done
+
+printf "All tests passed!\n\n"
+printf "Max time: %.3fs\n" $(( TIME_MAX ))e-3
+printf "Test name: %s\n\n" $TIME_MAX_NAME
