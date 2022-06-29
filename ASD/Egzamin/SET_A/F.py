bd271da257600c4c26af29b1e061f69f74bfe39d
Rusiek <szymon.rusiecki@protonmail.com>
2022-06-29 11:20:20 +0200

    Templates v.0.1

---
+++ ASD/Egzamin/SET_A/F.py
@@ -0,0 +1,16 @@
+anwser = {
+    0: ">",
+    1: "=",
+    2: "<"
+}
+
+# function should return:
+# 0 - if a > b
+# 1 - if a = b
+# 2 - if a < b
+def function(a, b):
+    # Enter code here
+    return 0
+
+if __name__ == "__main__":
+    print(anwser[function(input(), input())])
